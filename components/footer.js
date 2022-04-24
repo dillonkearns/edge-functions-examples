1bcdf474d4d5e65d9bf972450a429e62dd29b0cd
Dillon Kearns <dillon@incrementalelm.com>
2022-04-24 10:03:55 -0700

    Remove extra boilerplate.

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-export default function footer() {
-  const date = new Date();
-  const year = date.getFullYear();
-  return `
-    <footer class="footer">
-      <p class="footer__copyright">© ${year} Netlify</p>
-    </footer>
-  `;
-}
