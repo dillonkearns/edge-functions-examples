14f47c7f4eab2e73bd87b34d91a7e2ccc624d808
Dillon Kearns <dillon@incrementalelm.com>
2022-04-24 10:04:43 -0700

    Remove extra functions.

--- netlify/edge-functions/image-internal.ts
+++
@@ -1,8 +0,0 @@
-import type { Context } from "netlify:edge";
-
-// Let's serve an image that's stored in the repo
-// by rewriting the URL.
-
-export default async (request: Request, context: Context) => {
-  return context.rewrite("/apple-touch-icon.png");
-};
--- netlify/edge-functions/set-request-header.ts
+++
@@ -1,5 +0,0 @@
-import type { Context } from "netlify:edge";
-
-export default async (request: Request, context: Context) => {
-  request.headers.set("X-Your-Custom-Header", "Your custom header value");
-};
